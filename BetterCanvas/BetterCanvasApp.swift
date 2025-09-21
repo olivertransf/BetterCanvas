dc2176b665531ab121c1a781ee7fd54cf0a7ad31
Oliver Tran <olivertransf@gmail.com>
2025-09-21 13:12:14 -0700

    Initial Commit

---
+++ BetterCanvas/BetterCanvasApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterCanvasApp.swift
+//  BetterCanvas
+//
+//  Created by Oliver Tran on 9/21/25.
+//
+
+import SwiftUI
+
+@main
+struct BetterCanvasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
